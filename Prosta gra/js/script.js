5ff204a2b9a924386c47ecf5903d2a947cfc2609
Patryk Kalinowski <Patryk Kalinowski>
2016-10-17 19:30:15 +0200

    update

--- Prosta gra/js/script.js
+++ Prosta gra/js/script.js
@@ -1,7 +1,8 @@
+
 var btnNewGame = document.getElementById('js-startGame');
-var playground = document.getElementById('wrapper');
 btnNewGame.addEventListener('click', showForm, false);
 
+// This function is responsible for showing modal window
 function showForm() {
     var modalWindow = document.getElementById('modalWindow');
     var span = document.getElementById('close');
@@ -15,26 +16,63 @@ function showForm() {
         }
     }
 }
+
+/////////// DATA VALIDATION  ///////////////
+
 var form = document.getElementById('submit-form');
 var btnSubmit = document.getElementById('js-loadGame');
+var login="";
 
-
+//Data validation
 form.addEventListener('submit', function (e) {
     
-    console.log(e.target);
+ 
     var login = document.getElementById('login').value;
     var expression = /^[a-zA-Z0-9]{6,12}$/;
     if (login.match(expression)) {
         document.getElementById('message').style.display = "none";
+        loadPlayground(); //bardziej by pasowało wgrac to asynchronicznie
+        
     }
     else {
-//        e.preventDefault();
+        e.preventDefault();
         document.getElementById('message').style.display = "block";
     }
-        e.preventDefault();
-   
+//      e.preventDefault();
 });
+
 document.getElementById('reset').addEventListener('click', function () {
     document.getElementById('message').style.display = "none";
 });
+
+
+function loadPlayground() {
+    document.getElementById('introduction').style.display='none';
+    document.getElementById('js-playerPickElement').style.display='block';
+    btnNewGame.innerHTML = "Jeszcze raz";
+}
+
+var pickRock = document.getElementById('js-playerPick_rock'),
+     pickPaper = document.getElementById('js-playerPick_paper'),
+     pickScissors = document.getElementById('js-playerPick_scissors');
+
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
