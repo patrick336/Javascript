8238fde7003a2f8e232e17a3eaa5cd73c26bec51
Patryk Kalinowski <Patryk Kalinowski>
2016-10-22 00:37:15 +0200

    Organizating code and fixes

--- Prosta gra/js/script.js
+++ Prosta gra/js/script.js
@@ -1,26 +1,49 @@
-var   introduction = document.getElementById('introduction');
-var   newGameElem = document.getElementById('js-newGameElement'),
-      pickElem = document.getElementById('js-playerPickElement'),
-      resultsElem = document.getElementById('js-resultsTableElement');
+//////////////////  ZMIENNE GLOBALNE /////////////////////
 
-var   playerPointsElem = document.getElementById('js-playerPoints'),
-      playerNameElem = document.getElementById('js-playerName'),
-      computerPointsElem = document.getElementById('js-computerPoints');
+//Opis rozgrywki
+var introduction = document.getElementById('introduction');
 
+//Kontener przycisku nowej gry
+var newGameElem = document.getElementById('js-newGameElement');
+   
+// Tabela trzymająca dane, wybory graczy oraz wynik.
+var resultsElem = document.getElementById('js-resultsTableElement');
 
-var   pickRock = document.getElementById('js-playerPick_rock'),
-      pickPaper = document.getElementById('js-playerPick_paper'),
-      pickScissors = document.getElementById('js-playerPick_scissors');
+//Nick gracza oraz wynik bieżącej rozgrywki.
+var playerNameElem = document.getElementById('js-playerName'),
+    playerPointsElem = document.getElementById('js-playerPoints'),
+    computerPointsElem = document.getElementById('js-computerPoints');
 
-var   playerPickElem = document.getElementById('js-playerPick'),
-      computerPickElem = document.getElementById('js-computerPick'),
-      playerResultElem = document.getElementById('js-playerResult'),
-      computerResultElem = document.getElementById('js-computerResult'),
-      scoreless = document.getElementById('js-scoreless');
+//Kontener oraz poszczególne przyciski do gry.
+var pickElem = document.getElementById('js-playerPickElement'),
+    pickRock = document.getElementById('js-playerPick_rock'),
+    pickPaper = document.getElementById('js-playerPick_paper'),
+    pickScissors = document.getElementById('js-playerPick_scissors');
 
+//Wybory graczy.
+var playerPickElem = document.getElementById('js-playerPick'),
+    computerPickElem = document.getElementById('js-computerPick');
+    
+// Sygnalizacja zwycięstwa danej rundy.    
+var playerResultElem = document.getElementById('js-playerResult'),
+    computerResultElem = document.getElementById('js-computerResult');
+
+// Sygnalizacja remisu    
+var scoreless = document.getElementById('js-scoreless');
+
+// Ogłoszenie zwycięstwa całej rozgrywki.
+var resultGame = document.getElementById('resultGame');
+// Zmienne okna modalnego.
 var modalWindow = document.getElementById('modalWindow');
 var btnNewGame = document.getElementById('js-startGame');
-// Wartości początkowe
+
+// Zmienne formularza.
+var form = document.getElementById('submit-form'),
+    btnSubmit = document.getElementById('js-loadGame'),
+    message = document.getElementById('message');
+
+
+// Wartości początkowe.
 var gameState = 'notStarted'; 
 var player = {
         name: '',
@@ -29,6 +52,10 @@ var player = {
     computer = {
         score: 0
     };
+
+
+//////////////// PROCEDURY FUNKCJI ////////////////////////
+
 
 setGameElements();
 
@@ -54,10 +81,10 @@ function setGameElements() {
   }
 }
 
-
+// Wyświetlanie ruchu graczy w formie ikony.
 function showPick (movement) {
     
-    var iconMove = ['<i class="fa fa-hand-rock-o" aria-hidden="true">',
+    var iconMove = ['<i class="fa fa-hand-rock-o" aria-hidden="true"></i>',
                     '<i class="fa fa-hand-paper-o" aria-hidden="true"></i>',
                     '<i class="fa fa-hand-scissors-o" aria-hidden="true"></i>'];
     
@@ -79,14 +106,7 @@ function showPick (movement) {
     }
 }
 
-// NASŁUCHIWANIE WYBORU GRACZA, FUNKCJA DAJE INFORMACJE CO GRACZ WYBRAŁ
-pickRock.addEventListener('click', function() { playerPick('rock') });
-pickPaper.addEventListener('click', function() { playerPick('paper') });
-pickScissors.addEventListener('click', function() { playerPick('scissors') });
-
-
-
-// wygenerowanie wyboru komputera
+// Wygenerowanie wyboru komputera
 function getComputerPick () {
     var possiblePicks = ['rock', 'paper', 'scissors'];
     return possiblePicks [Math.floor(Math.random()*3)];
@@ -106,6 +126,8 @@ function playerPick(playerPick) {
     
     checkRoundWinner(playerPick, computerPick);
 }
+
+//Sprawdzanie rundy.
 function checkRoundWinner(playerPick, computerPick) {
   playerResultElem.innerHTML = computerResultElem.innerHTML = scoreless.innerHTML='';
 
@@ -132,17 +154,14 @@ function checkRoundWinner(playerPick, computerPick) {
     setGamePoints();
     whoWins();
 }
-//AKTUALIZACJA WYNIKU NA EKRANIE
+
+//AKTUALIZACJA WYNIKU NA EKRANIE.
 function setGamePoints() {
     playerPointsElem.innerHTML = player.score;
     computerPointsElem.innerHTML = computer.score;
 }
 
-// Zmienne okna modalnego oraz przycisku 'New Game'.
-
-btnNewGame.addEventListener('click', showForm, false);
-
-// POKAZYWANIE OKNA MODALNEGO I JEGO OBSŁUGA
+// POKAZYWANIE OKNA MODALNEGO I JEGO OBSŁUGA.
 function showForm() {
 
     var span = document.getElementById('close');
@@ -157,19 +176,46 @@ function showForm() {
     }
     window.onkeydown = function (event){
         if (event.keyCode == 27) 
-    // press ESC to close window        
+    // Naciśnięcie ESC zamyka okno       
             modalWindow.style.display = 'none';
     }
 }
 
 
 
-/////////// WALIDACJA DANYCH  ///////////////
+//Sprawdzanie ostatecznego wyniku 
+//i wyprowadzenie na ekran informacji o zwycięstwie.
+function whoWins () {
+    
+    if(player.score == 10) {
+        gameState = 'ended';
+        setGameElements();
+        resultGame.innerHTML = "Wygrał: " + player.name + ". Gratulacje !";
+    }
+    else if(computer.score == 10) {
+        gameState = 'ended';
+        setGameElements();
+        resultGame.innerHTML = "Wygrał komputer. Gramy rewanż?";
+    }
+    
+      
+}
 
-var form = document.getElementById('submit-form');
-var btnSubmit = document.getElementById('js-loadGame');
-var message = document.getElementById('message');
+//Resetowanie wartości przed rozgrywką.
+function resetValues () {
+    playerPickElem.innerHTML="";
+    computerPickElem.innerHTML="";
+    playerResultElem.innerHTML="";
+    computerResultElem.innerHTML="";    
+    player.score = computer.score = 0;
+    player.name = "";
+    gameState ='started';
+}
 
+//////////////// NASŁUCHIWANIE ZDARZEŃ ////////////////////////
+
+
+// Walidacja danych.
 form.addEventListener('submit', function (event) {
     
     resetValues();
@@ -191,37 +237,15 @@ form.addEventListener('submit', function (event) {
    event.preventDefault();
 });
 
-//usuwanie komunikatu
+//Przycisk nowej gry i pokazanie okna. 
+btnNewGame.addEventListener('click', showForm, false);
+
+//Usuwanie komunikatu.
 document.getElementById('reset').addEventListener('click', function () {
     message.style.display = "none";
 });
 
-
-var resultGame = document.getElementById('resultGame');
-function whoWins () {
-    
-    if(player.score == 10) {
-        gameState = 'ended';
-        setGameElements();
-        resultGame.innerHTML = "Wygrał: " + player.name + ". Gratulacje !";
-    }
-    if(computer.score == 10) {
-        gameState = 'ended';
-        setGameElements();
-        resultGame.innerHTML = "Wygrał komputer. Gramy rewanż?";
-    }
-    
-      
-}
-
-
-function resetValues () {
-    playerPickElem.innerHTML="";
-    computerPickElem.innerHTML="";
-    playerResultElem.innerHTML="";
-    computerResultElem.innerHTML="";    
-    player.score = computer.score = 0;
-    player.name = "";
-    gameState ='started';
-}
-
+// NASŁUCHIWANIE WYBORU GRACZA, FUNKCJA DAJE INFORMACJE CO GRACZ WYBRAŁ
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
